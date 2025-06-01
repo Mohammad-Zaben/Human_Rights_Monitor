2993e4c92aabd73bd518d0834381df27d1f36993
Zaben <141401082+Mohammad-Zaben@users.noreply.github.com>
2025-06-01 18:06:29 +0300

    tets

--- app/models/users.py
+++ app/models/users.py
@@ -12,7 +12,10 @@ class UserBase(BaseModel):
     username: str = Field(..., min_length=3, max_length=50)
     email: EmailStr
     role: UserType
+    phone: Optional[str] = Field(None, min_length=10, max_length=15)  
+    preferred_contact_method: Optional[str] = Field(None, max_length=50)  
     id: Optional[str] = None
+    organization_name: Optional[str] = Field(None, max_length=100)
 
 class UserCreate(UserBase):
     password: str = Field(..., min_length=8, max_length=128)
--- app/routes/cases.py
+++ app/routes/cases.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status,Form,UploadFile,File
+from fastapi import APIRouter, HTTPException, status,Form,UploadFile,File, Depends
 from app.database import get_collection
 from app.models.cases import CaseBase, CaseResponse
 from app.routes.evidence import create_evidence
@@ -6,6 +6,8 @@ from datetime import datetime
 from app.services.id_genarator import case_generate_id
 from pydantic import ValidationError
 import json
+from app.auth.oauth import get_current_user
+
 router = APIRouter(
     prefix="/case",
     tags=["Cases"],
@@ -19,7 +21,8 @@ router = APIRouter(
             response_description="The created case with generated ID")
 async def create_case(
     case_model: str = Form(..., example='{"title": "Test Case", "description": "A sample case", "violation_types": ["type1", "type2"], "status": "under_investigation", "priority": "high", "location": {"country": "Country", "region": "Region", "coordinates": {"type": "Point", "coordinates": [34.4667, 31.5000]}}, "date_occurred": "2025-06-01T12:00:00", "date_reported": "2025-06-01T12:00:00", "victims": ["victim1"], "perpetrators": [{"name": "Perpetrator", "type": "individual"}], "created_by": "user_id"}'),
-    images: list[UploadFile] = File(...)
+    images: list[UploadFile] = File(...),
+    current_user: str = Depends(get_current_user)
 ):
 
     try:
@@ -40,6 +43,7 @@ async def create_case(
     case_data = case.model_dump()
     case_data["evidence"] = images_list
     case_data["case_id"] = await case_generate_id()
+    case_data["created_by"] = current_user.username
 
     now = datetime.utcnow()
     case_data["created_at"] =  now
